d2cc1d666123ebddad95c77b1bd2493cce7510b0
Jonathan Perkins <jperkin4@uab.edu>
2024-07-29 15:59:09 -0500

    Creating login page and navbar

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="auth">
+      <h1>Welcome, Ruby.</h1>
+      <form>
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <button>Login</button>
+          <p>Sorry, wrong username and/or password</p>
+      </form>
+    </div>
   )
 }
 
