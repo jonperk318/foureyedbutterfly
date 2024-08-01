de76fbe77f4a2204fdd41d1b431df2750098c2d2
Jonathan Perkins <jperkin4@uab.edu>
2024-07-31 23:56:04 -0500

    Creating Home page animations

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,22 +8,22 @@ const Navbar = () => {
     <div className="navbar">
         <div className="container">
             <div className="links">
-                <Link className="link hvr-underline-from-left" to="/?cat=about">
+                <Link className="link hvr-underline-from-left link1" to="/?cat=about">
                     <h6>About</h6>
                 </Link>
-                <Link className="link hvr-underline-from-left" to="/?cat=2024">
+                <Link className="link hvr-underline-from-left link2" to="/?cat=2024">
                     <h6>2024</h6>
                 </Link>
-                <div className="logo">
+                <div className="logo link3">
                     <a href={"./"}>
                         <img src={Logo} alt="logo"/>
                     </a>
                 </div>
                 <span className="admin">
-                <Link className="link hvr-underline-from-left" to="/">Logout</Link>
+                <Link className="link hvr-underline-from-left link4" to="/">Logout</Link>
                 </span>
                 <span className="admin">
-                <Link className="link hvr-underline-from-left" to="/write">Write</Link>
+                <Link className="link hvr-underline-from-left link5" to="/write">Write</Link>
                 </span>
                 <div className="insta link">
                     <a href={"https://www.instagram.com/foureyedbutterfly/?next=%2F"}>
