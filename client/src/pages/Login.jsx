e27b0a4cff40c6a9d037415b615f766ac1d8ed34
Jonathan Perkins <jperkin4@uab.edu>
2024-08-04 18:59:13 -0500

    Creating write page and editing login page
    and fixing navbar... again

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,13 +3,15 @@ import React from 'react'
 const Login = () => {
   return (
     <div className="auth">
-      <h1>Welcome, Ruby.</h1>
-      <form>
-        <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" />
-        <button>Login</button>
-          <p>Sorry, wrong username and/or password</p>
-      </form>
+        <div className="login-container">
+          <h1>Welcome, Ruby.</h1>
+          <form>
+            <input type="text" placeholder="username" />
+            <input type="password" placeholder="password" />
+            <button>Login</button>
+              <p>Sorry, wrong username and/or password</p>
+          </form>
+        </div>
     </div>
   )
 }
--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const Write = () => {
+
+    const [value, setValue] = useState('');
+    console.log(value);
+
   return (
-    <div>Write</div>
+    <div className="write-post">
+        <div className="write-container">
+          <div className="content">
+              <input type="text" placeholder="Title" />
+              <div className="editor-container">
+                  <ReactQuill className="editor" theme="snow" value={value} onChange={setValue} />
+              </div>
+          </div>
+          <div className="menu">
+              <div className="item">
+                  <h1>Publish</h1>
+                  <span>
+                    <b>Status: </b> Draft
+                </span>
+                  <span>
+                    <b>Visibility: </b> Public
+                </span>
+                  <input style={{display:"none"}} type="file" id="file" name="" />
+                  <label htmlFor="file">Upload Image or Video</label>
+                  <div className="buttons">
+                      <button>Save as a draft</button>
+                      <button>Update</button>
+                  </div>
+              </div>
+          </div>
+        </div>
+    </div>
   )
 }
 
