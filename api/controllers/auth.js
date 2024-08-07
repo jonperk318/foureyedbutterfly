b400a5b61bc70e177b8cfdb815d35c9769304dd3
Jonathan Perkins <jperkin4@uab.edu>
2024-08-07 01:41:12 -0500

    Creating login and logout with cookies

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,6 @@
 import {db} from "../db.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
 
@@ -14,9 +15,9 @@ export const register = (req, res) => {
 
         // Check admin password
 
-        const q = "SELECT * FROM users WHERE username = ?;";
+        const q = "SELECT * FROM users WHERE id = ?;";
 
-        db.query(q, ["jonperk318"], (err, data) => {
+        db.query(q, [4], (err, data) => {
             if (err) return res.status(500).json(err);
 
             const isAdminPasswordCorrect = bcrypt.compareSync(
@@ -47,8 +48,31 @@ export const register = (req, res) => {
 
 export const login = (req, res) => {
 
+    // Check if user exists
+
+    const q = "SELECT * FROM users WHERE username = ?"
+
+    db.query(q, [req.body.username], (err, data) => {
+        if (err) return res.status(500).json(err);
+        if (data.length === 0) return res.status(404).json("User not found");
+
+        // Check password
+
+        const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password);
+
+        if (!isPasswordCorrect) return res.status(400).json("Wrong username or password");
+
+        const token = jwt.sign({id:data[0].id}, "jwtkey");
+        const {password, ...other} = data[0] // separate password from other information
+
+        res.cookie("access_token", token, {httpOnly:true}).status(200).json(other);
+    })
+
 }
 
 export const logout = (req, res) => {
-
-}
+    res.clearCookie("access_token",{
+      sameSite:"none",
+      secure:true
+    }).status(200).json("Come again soon!")
+  };
