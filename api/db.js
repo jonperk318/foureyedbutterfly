47c0cbd2b192e1c660d374999c2340fc6bc0b2e8
Jonathan Perkins <jperkin4@uab.edu>
2024-08-09 16:12:26 -0500

    Creating 404 page and fixing styling quirks

--- api/db.js
+++ api/db.js
@@ -3,6 +3,6 @@ import mysql from 'mysql2';
 export const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'what are you looking at',
+    password: 'yeah you',
     database: 'four-eyed-butterfly'
 })
--- client/src/pages/Posts2024.jsx
+++ client/src/pages/Posts2024.jsx
@@ -3,33 +3,6 @@ import {Link} from 'react-router-dom'
 import { useEffect } from "react";
 import axios from "axios";
 
-/*
-const Posts = [
-    {
-        id: 2,
-        title: "All the World's a Stage",
-        date: "2/11/24",
-        img: IMG1,
-        content: "Offline by Alex Kozobolis\n" +
-            "\n" +
-            "I think ambition can be something of an impacted tooth. It grows with small movements underneath the disguise of having more time and then suddenly, without your expectation, you are under amnesia soon to be washing blood and throw-up out of your gum holes.",
-        endImg: IMG1a
-    },
-    {
-        id: 1,
-        title: "Enjoying the Mundane",
-        date: "2/9/24",
-        img: IMG2,
-        content: "The Secret Life Of Daydreams by Dario Marianelli \n" +
-        "\n" +
-        "I’m not a creature of habit and if you say you are, I envy you. \n" +
-        "\n" +
-        "Of course there has to be some consistency in life, or else you’ll fall off the edge. And then of course, some people like that falling feeling. I am not one of those people either. ",
-        endImg: IMG2a
-    }
-]
-*/
-
 const Posts2024 = () => {
 
     const [Posts, setPosts] = useState([]);
