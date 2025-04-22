335f577037aac0f0bda425503a0e2ceb5d080e32
Jonathan Perkins <jperkin4@uab.edu>
2025-04-21 22:18:59 -0400

    Tweak to css

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
-import { errorUtils } from "../utils/errorUtils";
+import { errorUtils } from "@/utils/errorUtils";
 
 
 const Register = () => {
