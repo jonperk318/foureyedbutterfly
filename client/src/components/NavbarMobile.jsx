1b3d83e744ef522f43517e0595bb83c51e3c59f9
Jonathan Perkins <jonperk318@gmail.com>
2025-05-09 09:27:17 -0400

    Fixed hamburger menu

--- client/src/components/NavbarMobile.jsx
+++ client/src/components/NavbarMobile.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import { FaInstagram } from "react-icons/fa";
 import { IconContext } from "react-icons";
 import { useState } from "react";
-import { Squash as Hamburger } from "hamburger-react";
 
 import { AuthContext } from "@/context/authContext.jsx";
 
@@ -42,19 +41,26 @@ const NavbarMobile = () => {
           </Link>
         </div>
       </div>
-      <div className="burger">
-        <svg class="burger rotate" viewBox="0 0 100 100" width="80" onclick="this.classList.toggle('active')">
-          <path
-                class="line top"
-                d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20" />
-          <path
-                class="line middle"
-                d="m 30,50 h 40" />
-          <path
-                class="line bottom"
-                d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20" />
+      <div className="menu">
+        <svg
+          className={"burger " + (isOpen && " active")}
+          viewBox="0 0 100 100"
+          width="75"
+          toggled={isOpen.toString()}
+          onClick={toggleBurger}
+        >
+          <a>
+            <path
+              className="line top"
+              d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"
+            />
+            <path className="line middle" d="m 30,50 h 40" />
+            <path
+              className="line bottom"
+              d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"
+            />
+          </a>
         </svg>
-        <Hamburger toggled={isOpen} size={30} toggle={toggleBurger} />
       </div>
       {isOpen && (
         <div className="navbar-container-open">
